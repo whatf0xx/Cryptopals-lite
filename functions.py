4f3852f8618bd08b2078af633f212aa243f2d13c
whatf0xx <whatf0xx@gmail.com>
2022-08-06 22:52:54 +0100

    Wrote the function for task 2

--- functions.py
+++ functions.py
@@ -5,8 +5,7 @@ def hex_to_base64(h: str) -> str:
     hex_bytes = binascii.a2b_hex(h)
     return base64.encodebytes(hex_bytes)[:-1].decode()
 
-def main() -> None:
-    print(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
-
-if __name__ == "__main__":
-    main()
+def XOR_combo(a: str, b: str) -> str:
+    a_int = int(a, 16)
+    b_int = int(b, 16)
+    return hex(a_int ^ b_int)
