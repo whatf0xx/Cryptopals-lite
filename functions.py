32f6be2107950586cc22eb3c3cf05bb113ee5a55
whatf0xx <whatf0xx@gmail.com>
2022-08-23 15:47:14 +0200

    Refactored the XORing into a single, neat function

--- functions.py
+++ functions.py
@@ -1,28 +1,7 @@
 import base64
 import pickle
 from typing import List, Tuple
-
-def get_frequencies(s: str, spaces=True):
-    alphabet = "abcdefgijklmnopqrstuvwxyz "
-    if spaces:
-        alphabet += " "
-
-    frequency_dict =  {a:s.count(a) for a in alphabet}
-    return {a:frequency_dict[a] / sum(frequency_dict.values()) for a in alphabet}  # normalisation
-
-def byte_XOR_break(s: str):
-    with open("frequency_dict_spaces.pkl", 'rb') as f:
-        frequency_dict_spaces = pickle.load(f)
-
-    keys = [("0" * (2-len(hex(a)[2:])) + hex(a)[2:]).encode() for a in range(256)] # all possible byte keys
-    for k in keys:
-        print(k)
-        guess_text = byte_XOR_decrypt(s, k)
-
-def vig_encrypt(s: str, key: str, output_encoding: str="base64") -> str:
-    return None
-
-
+from itertools import cycle
 
 def to_bytes(s: str, encoding: str = "hex") -> List[str]:
     match encoding:
@@ -50,15 +29,16 @@ def hex_XOR(s: str, t: str) -> str:
     """
     return hex(int(s, 16) ^ int(t, 16))
 
+def key_XOR(buffer: List[str], key: List[str]) -> List[str]:
+    return  [hex_XOR(p, k) for p, k in zip(buffer, cycle(key))]
+
 def byte_XOR_encrypt(s: str, c: str, output_encoding="b64") -> str:
     """
     Encrypts 's', a plaintext string, using the single byte key, 'c'. 'c' should be passed as a string-represented hexadecimal number between 0 and 255, inclusive, obviously.
     """
     string_buffer = to_bytes(s, 'plaintext')
-    key_buffer = [c] * len(string_buffer)
-
     
-    output_buffer = [hex_XOR(p, k) for p, k in zip(string_buffer, key_buffer)]
+    output_buffer = key_XOR(string_buffer, [c])
 
     output_dict = {"b64": to_b64(output_buffer),
                     "hex": to_hex(output_buffer)}
@@ -70,8 +50,29 @@ def byte_XOR_decrypt(s: str, c: str, input_encoding="b64"):
                     "hex" : to_bytes(s)}
     
     cipher_buffer = input_dict[input_encoding]
-    key_buffer = [c] * len(cipher_buffer)
 
-    output_buffer = [hex_XOR(c, k) for c, k in zip(cipher_buffer, key_buffer)]
+    output_buffer = key_XOR(cipher_buffer, [c])
 
     return to_plaintext(output_buffer)
+
+def get_frequencies(s: str, spaces=True):
+    alphabet = "abcdefgijklmnopqrstuvwxyz"
+    if spaces:
+        alphabet += " "
+
+    frequency_dict =  {a:s.count(a) for a in alphabet}
+    return {a:frequency_dict[a] / sum(frequency_dict.values()) for a in alphabet}  # normalisation
+
+def byte_XOR_break(s: str):
+    with open("frequency_dict_spaces.pkl", 'rb') as f:
+        frequency_dict_spaces = pickle.load(f)
+
+    keys = [("0" * (2-len(hex(a)[2:])) + hex(a)[2:]).encode() for a in range(256)] # all possible byte keys
+    for k in keys:
+        print(k)
+        guess_text = byte_XOR_decrypt(s, k)
+
+if __name__ == "__main__":
+    buffer = to_bytes("Hello", "plaintext")
+    key = to_bytes("AB", "plaintext")
+    print(key_XOR(buffer, key))
